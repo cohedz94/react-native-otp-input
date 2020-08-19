588877d23ec86e7d2f4e455b7d38becab7aba1c4
Hungnm <hung.nguyen@med247.co>
2020-08-19 14:31:48 +0700

    fix clear otp when resend

--- index.d.ts
+++ index.d.ts
@@ -54,10 +54,6 @@ declare module '@twotalltotems/react-native-otp-input' {
      */
     secureTextEntry?: boolean;
     /**
-     * Set editable for inputs
-     */
-    editable?: boolean;
-    /**
      * Type of the keyboard
      */
     keyboardType?: KeyboardType;
